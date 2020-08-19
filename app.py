6a34b26947063eb4af32ddd3abc1c8fa955ed07b
maxtillberg <max.tillberg@bengtdahlgren.se>
2020-08-19 20:26:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ import pandas as pd
 
 ########### Get Data
 weatherdata = pd.read_csv('weather.csv', encoding = "cp1252")
-
-
-df = px.data.carshare()
+df = pd.read_csv(url, encoding = "cp1252")
+fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="Dry bulb max summer", size="Time zone",
+                  color_continuous_scale=px.colors.diverging.Portland, hover_name="Name", size_max=9, zoom=3,
+                  mapbox_style="carto-positron")
 
 app = dash.Dash(
     __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
