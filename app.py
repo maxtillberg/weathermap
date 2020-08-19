f6c622ac78b3bf538a069202dfb7bf33547e1280
maxtillberg <max.tillberg@bengtdahlgren.se>
2020-08-19 17:02:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,16 @@ fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon", color="peak_
                   color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10,
                   mapbox_style="carto-positron")
 
-app = dash.Dash()
+app = dash.Dash(
+    __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+)
+server = app.server
+
+#app = dash.Dash()
 app.layout = html.Div([
     dcc.Graph(figure=fig)
 ])
 
-app.run_server(debug=True, use_reloader=False) 
+#app.run_server(debug=True, use_reloader=False) 
+if __name__ == "__main__":
+    app.run_server(debug=True)
