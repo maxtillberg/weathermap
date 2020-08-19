a89eb3768f6b6f74e6272e56755b97d4162b5dd8
maxtillberg <max.tillberg@bengtdahlgren.se>
2020-08-19 17:35:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-import plotly.graph_objects as go # or plotly.express as px
+import plotly.express as px
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
 
-mapbox_access_token = open(".mapbox_token").read()
-
-fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon", color="peak_hour", size="car_hours",
-                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10,
-                  mapbox_style="carto-positron")
+df = px.data.carshare()
 
 app = dash.Dash(
     __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 )
 server = app.server
 
-#app = dash.Dash()
-app.layout = html.Div([
+fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon", color="peak_hour", size="car_hours",
+                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10,
+                  mapbox_style="carto-positron")
+
+app.layout = html.Div(
+    [
+        html.H1("IDA ICE Weather files"),
+        html.Div([
     dcc.Graph(figure=fig)
 ])
 
-#app.run_server(debug=True, use_reloader=False) 
 if __name__ == "__main__":
     app.run_server(debug=True)
